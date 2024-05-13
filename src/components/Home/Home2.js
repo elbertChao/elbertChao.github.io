7a2f61687f020969eebc81a05429794399d339d4
echao <echao8@uwo.ca>
2024-05-13 14:03:36 -0400

    initial introduction lines in progress

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,36 +19,35 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              
+              Welcome to my portfolio! 🚀
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I am a passionate
               <i>
-                <b className="purple"> Java, Javascript. </b>
+                <b className="purple"> Software Developer </b>
+                that fell in love with solving problems and
+                <b className="purple"> building things that can make a difference.</b>
+              </i>
+              <br />
+              <br />I usually code in 
+              <i>
+                <b className="purple"> Javascript and Python</b>
+                , but I have also worked with other languages like
+                <b className="purple"> C++ and Java</b>
+                .
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              I have experience in building web applications with the MERN Stack
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple"> (MongoDB, Express, React, Node.js) </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              My passion lies in&nbsp;
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple">Web Technologies and AI </b>
               </i>
             </p>
           </Col>
