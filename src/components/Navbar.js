42d0eb2a1c64a48cab4af473e99f90ad6e33f24f
echao <echao8@uwo.ca>
2024-06-04 16:33:52 -0400

    Changing github navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/elbertChao/echaoPortfolio"
+                href="https://github.com/elbertChao/elbertChao.github.io"
                 target="_blank"
                 className="fork-btn-inner"
               >
